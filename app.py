53c8a4b832564240970304a80726690ec8cfd3a6
Marcos Verdi <70574457+Marcos-Verdi@users.noreply.github.com>
2024-02-29 23:36:10 -0600

    commit made 02/29

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def hello():
-    return 'Hello, World!'
+    return render_template('market.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
